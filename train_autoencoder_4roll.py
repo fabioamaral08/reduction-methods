6984c58fd44403c6f75c327ea4a3c298496cb8c7
fabioamaral08 <fabiovini08@hotmail.com>
2024-02-27 12:14:40 -0800

    Multiple Training 4roll
    Add training for different parameters for the 4roll case

--- train_autoencoder_4roll.py
+++ train_autoencoder_4roll.py
@@ -15,81 +15,88 @@ if __name__ == '__main__':
     device_type = "cuda" if torch.cuda.is_available() else "cpu"
     device = torch.device(device_type)
 
+    parameters = [
+          (1,4.5,0.1),
+          (1,5.0,0.3),
+          (1,5.5,0.9),
+          (1,7.0,0.3),
+          (1,7.5,0.6),
+    ]
     latent_dim = 2
-    ## Data reading
 
-    # Parameters:
-    Re = 1
-    Wi = 5
-    beta = 0.1
-    # type of simulation
-    case = '4roll'
-    #read file
-    X, Xmean = get_data(Re,Wi,beta, case, n_data= -2, dir_path='../EnergyReduction/npz_data')
+    for params in parameters:
+        ## Data reading
 
-    Nt = X.shape[1] # number of snapshots
-    X_data = X.reshape((-1,5,Nt))
-    X_data = np.moveaxis(X_data,[0,2],[2,0]) # (Nx, Nc, Nt) -> (Nt, Nc, Nx)
+        # Parameters:
+        Re ,Wi ,beta = params
+        # type of simulation
+        case = '4roll'
+        #read file
+        X, Xmean = get_data(Re,Wi,beta, case, n_data= -2, dir_path='../EnergyReduction/npz_data')
 
-    # convert data
-    X_torch = torch.from_numpy(X_data)
+        Nt = X.shape[1] # number of snapshots
+        X_data = X.reshape((-1,5,Nt))
+        X_data = np.moveaxis(X_data,[0,2],[2,0]) # (Nx, Nc, Nt) -> (Nt, Nc, Nx)
 
-    #normalize data inside autoencoder
-    lower_bound = torch.from_numpy(X_data.min(axis = (0,2)).reshape((1,5,1))).float().to(device)
-    upper_bound = torch.from_numpy(X_data.max(axis = (0,2)).reshape((1,5,1))).float().to(device)
-    # X_torch = (X_torch - lower_bound)/(upper_bound - lower_bound)
+        # convert data
+        X_torch = torch.from_numpy(X_data)
 
-    # NN part
-    learning_rate = 1e-4
-    bs = 100
-    num_epochs = 5000
+        #normalize data inside autoencoder
+        lower_bound = torch.from_numpy(X_data.min(axis = (0,2)).reshape((1,5,1))).float().to(device)
+        upper_bound = torch.from_numpy(X_data.max(axis = (0,2)).reshape((1,5,1))).float().to(device)
+        # X_torch = (X_torch - lower_bound)/(upper_bound - lower_bound)
 
-    autoencoder = Autoencoder.AutoencoderModule(n_input= X_torch.shape[-1], latent_dim = latent_dim, max_in=upper_bound, min_in=lower_bound).to(device)
+        # NN part
+        learning_rate = 1e-4
+        bs = 100
+        num_epochs = 5000
 
-    X_torch = X_torch.float().to(device)
-    dataset = TensorDataset(X_torch,X_torch)
-    loader = DataLoader(dataset, shuffle= True, batch_size=bs)
-    loss_fn = torch.nn.MSELoss()
-    optimizer = torch.optim.Adam(autoencoder.parameters(),lr = learning_rate)
+        autoencoder = Autoencoder.AutoencoderModule(n_input= X_torch.shape[-1], latent_dim = latent_dim, max_in=upper_bound, min_in=lower_bound).to(device)
 
-    num_batches = len(loader)
+        X_torch = X_torch.float().to(device)
+        dataset = TensorDataset(X_torch,X_torch)
+        loader = DataLoader(dataset, shuffle= True, batch_size=bs)
+        loss_fn = torch.nn.MSELoss()
+        optimizer = torch.optim.Adam(autoencoder.parameters(),lr = learning_rate)
 
-    # Results directory
-    pasta = f'ModelsTorch/Dense_Latent_{latent_dim}'
-    os.makedirs(pasta, exist_ok=True)
+        num_batches = len(loader)
 
-    # Early stop
-    best_vloss = 1_000_000
-    last_loss = best_vloss
-    patience = 0
-    #training
-    autoencoder.train(True)
-    for e in range(num_epochs):
-        if last_loss < best_vloss:
-                        best_vloss = last_loss
-                        torch.save({'optimizer_state_dict':optimizer.state_dict(), 'loss':loss, 'epoch':e}, f'{pasta}/optimizer_checkpoint.pt')
-                        torch.save(autoencoder.state_dict(), f'{pasta}/best_autoencoder')
-                        patience = 0
-        else:
-            patience += 1
-        if patience > 50:
-            autoencoder.load_state_dict(torch.load( f'{pasta}/best_autoencoder'))
-            break
+        # Results directory
+        pasta = f'ModelsTorch/Dense_Latent_{latent_dim}/Re{Re}_Wi{Wi}_beta{beta}'
+        os.makedirs(pasta, exist_ok=True)
 
-        cumm_loss = 0
-        t = time.time()
-        for data,_ in loader:
-            optimizer.zero_grad()
+        # Early stop
+        best_vloss = 1_000_000
+        last_loss = best_vloss
+        patience = 0
+        #training
+        autoencoder.train(True)
+        for e in range(num_epochs):
+            if last_loss < best_vloss:
+                            best_vloss = last_loss
+                            torch.save({'optimizer_state_dict':optimizer.state_dict(), 'loss':loss, 'epoch':e}, f'{pasta}/optimizer_checkpoint.pt')
+                            torch.save(autoencoder.state_dict(), f'{pasta}/best_autoencoder')
+                            patience = 0
+            else:
+                patience += 1
+            if patience > 50:
+                autoencoder.load_state_dict(torch.load( f'{pasta}/best_autoencoder'))
+                break
 
-            reconst = autoencoder(data)
-            loss = loss_fn(data, reconst)
-            loss.backward()
-            optimizer.step()
+            cumm_loss = 0
+            t = time.time()
+            for data,_ in loader:
+                optimizer.zero_grad()
 
-            cumm_loss += loss.item()
-        t = time.time() - t
-        last_loss = cumm_loss
-        print(f'Epoch {e}: running loss: {cumm_loss:.4f}')
-        print(f'Exec. Time of epoch: {t:.3f}s({t/num_batches:.3f}s/batch)\n')
+                reconst = autoencoder(data)
+                loss = loss_fn(data, reconst)
+                loss.backward()
+                optimizer.step()
 
-    torch.save(autoencoder,f'{pasta}/autoencoder_model')
+                cumm_loss += loss.item()
+            t = time.time() - t
+            last_loss = cumm_loss
+            print(f'Epoch {e}: running loss: {cumm_loss:.4f}')
+            print(f'Exec. Time of epoch: {t:.3f}s({t/num_batches:.3f}s/batch)\n')
+
+        torch.save(autoencoder,f'{pasta}/autoencoder_model')
