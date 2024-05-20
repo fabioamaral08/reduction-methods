2f90a717b9204edca521a0c68bf6727d6e21310a
fabioamaral08 <fabiovini08@hotmail.com>
2024-05-20 16:01:13 -0700

    Update train_vae_4roll_pred.py

--- train_scripts/train_vae_4roll_pred.py
+++ train_scripts/train_vae_4roll_pred.py
@@ -170,7 +170,7 @@ if __name__ == '__main__':
     bs = 2000
     num_epochs = 5000
 
-    autoencoder = Autoencoder.ParametricVAE(n_input= train_dataset[0][0].shape[-1],latent_dim = latent_dim, num_params=3, max_in=upper_bound, min_in=lower_bound, small = True, pred=True).to(device)
+    autoencoder = Autoencoder.ParametricVAEModule(n_input= train_dataset[0][0].shape[-1],latent_dim = latent_dim, num_params=3, max_in=upper_bound, min_in=lower_bound, small = True, pred=True).to(device)
 
     # sampler = CaseSampler(train_dataset.filenames, train_dataset.cases, train_dataset.root_dir)
     batch_sampler_train = CaseBatchSampler(train_dataset.filenames, train_dataset.cases, train_dataset.root_dir, bs)
